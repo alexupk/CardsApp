a986104427fe686c0f4761e7ce96bfc1f7415268
Oleksandr Nakorenok <alexandrnakorenok@gmail.com>
2024-08-02 16:43:17 +0300

    Action card part 1

--- components/ActionCard.tsx
+++ components/ActionCard.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, Linking } from 'react-native'
+import { Text, StyleSheet, View, Linking, Image, TouchableOpacity } from 'react-native'
 
 export default class ActionCard extends Component {
     render() {
@@ -9,7 +9,28 @@ export default class ActionCard extends Component {
         return (
             <View>
                 <Text style={styles.hedingText}> Action Card </Text>
-                <View style={[styles.card, styles.cardEleveted]}></View>
+                <View style={[styles.card, styles.cardEleveted]}>
+                    <View style={styles.hedingContainer}>
+                        <Text style={styles.headerText}>
+                            What's new in JavaScript 21 - ES12
+                        </Text>
+                    </View>
+                    <Image source={{uri: "https://images.stockcake.com/public/f/f/f/fffb12ac-656e-4b11-aa97-00b176ee1350_large/coding-late-night-stockcake.jpg"}}
+                    style={styles.cardImage}
+                    />
+                    <View style={styles.bodyContainer}>
+                        <Text numberOfLines={3}>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                            Perspiciatis deserunt esse minus totam unde assumenda, fugiat tempora beatae, expedita repellat 
+                            cum similique dolores debitis. Incidunt, aliquid! Dolore quidem nam quae?
+                        </Text>
+                    </View>
+                    <View style={styles.footerContainer}>
+                        <TouchableOpacity onPress={() => openWebSite("https://reactnative.dev/docs/linking")}>
+                            <Text style={styles.linkCard}>Read more</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
             </View>
         )
     }
@@ -21,6 +42,31 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         paddingHorizontal: 10
     },
-    card: {},
-    cardEleveted: {}
+    card: {
+        marginHorizontal: 12,
+        backgroundColor: "#FFF8DC"
+    },
+    cardEleveted: {},
+    hedingContainer: {
+        height:40,
+        flexDirection:"row",
+        justifyContent: "center",
+        alignItems:"center"
+    },
+    headerText: {
+        color: "#000000",
+        fontSize:16,
+        fontWeight:"600"
+    },
+    cardImage: {
+        height: 180
+    },
+    bodyContainer:{
+    },
+    footerContainer:{
+    },
+    linkCard:{
+    }
 })
+
+// 9 хв.
